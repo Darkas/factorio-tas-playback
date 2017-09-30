ef16612f5d51768411b3a3873d094c881e2b48c4
unique_2 <deep.blueeee@yahoo.de>
2017-09-30 14:49:31 +0200

    --

--- scenarios/hll-demo/blueprint_list.lua
+++ scenarios/hll-demo/blueprint_list.lua
@@ -6,7 +6,11 @@ local list = {
 local blueprints = {}
 
 for _, name in pairs(list) do
-  blueprints[name] = require("bp." .. name)
+	pcall(function() blueprints[name] = require("bp." .. name) end)
+	if not blueprints[name] then
+		if not global.blueprint_error then global.blueprint_error = {} end
+		global.blueprint_error[#global.blueprint_error + 1] = name
+	end
 end
 
 return blueprints
