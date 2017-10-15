723b6a0d81232fef21f4c95a658e4721fd8acaa5
Andre Knispel <andre.knispel@gmx.de>
2017-10-15 02:20:12 +0200

    Unified the move commands and added another option for auto-refuel.

--- high_level_commands.lua
+++ high_level_commands.lua
@@ -8,110 +8,6 @@ global.high_level_commands = global.high_level_commands or {
 	command_requests = {},
 }
 
-
-
-function auto_move_to_low_level (command, myplayer, tick)
-	if (not command.data.target_pos or not command.data.move_started) then
-		if command[1] == "move"  then
-			if not command.data.move_to_command then
-				command.data.target_pos = command[2]
-			else
-				command.data.target_pos = {}
-				command.data.target_pos = closest_point(command.data.target_command.rect, command.data.target_command.distance, myplayer.position)
-			end
-		else
-			command.data.target_pos = closest_point(command.rect, command.distance, myplayer.position)
-		end
-	end
-
-	if not command.data.last_dir then
-		command.data.last_dir = ""
-
-		if myplayer.position.y > command.data.target_pos[2] then
-			command.data.last_dir = command.data.last_dir .. "N"
-		end
-		if myplayer.position.y < command.data.target_pos[2] then
-			command.data.last_dir = command.data.last_dir .. "S"
-		end
-		if myplayer.position.x > command.data.target_pos[1] then
-			command.data.last_dir = command.data.last_dir .. "W"
-		end
-		if myplayer.position.x < command.data.target_pos[1] then
-			command.data.last_dir = command.data.last_dir .. "E"
-		end
-	end
-
-	local epsilon = 0.15 -- TODO: This should depend on the velocity.
-
-	if myplayer.position.y > command.data.target_pos[2] + epsilon then
-		command.data.move_north = true
-	end
-	if myplayer.position.y < command.data.target_pos[2] - epsilon then
-		command.data.move_south = true
-	end
-	if myplayer.position.x > command.data.target_pos[1] + epsilon then
-		command.data.move_west = true
-	end
-	if myplayer.position.x < command.data.target_pos[1] - epsilon then
-		command.data.move_east = true
-	end
-
-	if myplayer.position.y < command.data.target_pos[2] then
-		command.data.move_north = false
-	end
-	if myplayer.position.y > command.data.target_pos[2] then
-		command.data.move_south = false
-	end
-	if myplayer.position.x < command.data.target_pos[1] then
-		command.data.move_west = false
-	end
-	if myplayer.position.x > command.data.target_pos[1] then
-		command.data.move_east = false
-	end
-
-	command.data.move_dir = ""
-
-	if command.data.move_north then
-		command.data.move_dir = command.data.move_dir .. "N"
-	end
-	if command.data.move_south then
-		command.data.move_dir = command.data.move_dir .. "S"
-	end
-	if command.data.move_west then
-		command.data.move_dir = command.data.move_dir .. "W"
-	end
-	if command.data.move_east then
-		command.data.move_dir = command.data.move_dir .. "E"
-	end
-
-	return ""
-end
-
-function auto_move_execute(command, myplayer, tick)
-	if (command.data.move_to_command and in_range(command.data.target_command, myplayer)) or (command[1] == "move-to-entity" and in_range(command, myplayer)) then
-		command.finished = true
-		return {"phantom"}
-	end
-
-	if command.data.move_dir == "" then
-		if (command[1] == "move" and not command.data.move_to_command) or command[1] == "move-to-entity" then
-			command.finished = true
-			return {"phantom"}
-		else
-			command.data.move_dir = command.data.last_dir
-		end
-	end
-
-	if not command.data.move_started then
-		debugprint("Auto move to: " .. serpent.block(command.data.target_pos))
-		command.data.move_started = true
-	end
-
-	command.data.last_dir = command.data.move_dir
-
-	return {"move", command.data.move_dir}
-end
-
 function can_craft(craft, myplayer, need_intermediates)
 	if not myplayer.force.recipes[craft.name].enabled then
 		return false
@@ -309,7 +205,8 @@ high_level_commands = {
 			target = {"nil", "number"},
 			min = {"nil", "number"},
 			skip_coal_drills = "boolean",
-			type = {"nil", "string"}
+			type = {"nil", "string"},
+			pos = {"nil", "position"}
 		},
 		execute = empty,
 		spawn_commands = function(command, myplayer, tick)
@@ -327,7 +224,7 @@ high_level_commands = {
 					game.print("Invalid entity in auto-refuel! This may occur if you mine a fuelable entity.")
 				else
 					if not (command.skip_coal_drills and entity.type == "mining-drill" and entity.mining_target and entity.mining_target.name == "coal") then
-						if (not command.type) or entity.type == command.type then
+						if ((not command.type) or entity.type == command.type) and ((not command.pos) or (entity.position.x == command.pos[1] and entity.position.y == command.pos[2])) then
 							if entity.type == "mining-drill" then
 								priority = 4
 							end
@@ -670,63 +567,142 @@ high_level_commands = {
 		default_action_type = action_types.selection,
 	},
 
-	["move-to-entity"] = {
-		type_signature = {
-			[2] = "position",
-		},
-		execute = auto_move_execute,
-		executable = function(command, myplayer, tick)
-			if not command.data.target_entity then
-				command.data.target_entity = get_entity_from_pos(command[2], myplayer)
-				if not command.data.target_entity then
-					return "No entity at " .. serpent.block(command.position) .. "."
-				else
-					command.rect = collision_box(command.data.target_entity)
-				end
-			end
-			return auto_move_to_low_level(command, myplayer, tick)
-		end,
-		default_priority = 7,
-		initialize = function (command, myplayer)
-			command.data.move_north = false
-			command.data.move_south = false
-			command.data.move_west = false
-			command.data.move_east = false
-
-			command.distance = myplayer.reach_distance
-		end
-	},
-
 	move = {
 		type_signature = {
 			[2] = {"string", "position"},
 		},
-		execute = auto_move_execute,
-		executable = function(command, myplayer, tick)
-			if not command.data.move_to_command then return auto_move_to_low_level(command, myplayer, tick) end
+		execute = function (command, myplayer, tick)
+			if (command.data.move_to_command and in_range(command.data.target_command, myplayer)) or (command.data.move_to_entity and in_range(command, myplayer)) then
+				command.finished = true
+				return {"phantom"}
+			end
 
-			if not command.data.target_command then
-				for _, com in pairs(global.command_list_parser.current_command_set) do
-					if com.name and has_value({command[2], command.namespace .. command[2]}, com.namespace .. com.name) then
-						command.data.target_command = com
-					end
+			if command.data.move_dir == "" then
+				if (command[1] == "move" and not command.data.move_to_command) or command.data.move_to_entity then
+					command.finished = true
+					return {"phantom"}
+				else
+					command.data.move_dir = command.data.last_dir
 				end
 			end
 
-			if not command.data.target_command then
-				errprint("move-to-command: There is no command named: " .. command[2])
-				return "There is no command named: " .. command[2]
+			if not command.data.move_started then
+				debugprint("Auto move to: " .. serpent.block(command.data.target_pos))
+				command.data.move_started = true
 			end
 
-			if command.data.target_command[1] == "craft-build" and command.data.target_command.data.build_command then
-				command.data.target_command = command.data.target_command.data.build_command
+			command.data.last_dir = command.data.move_dir
+
+			return {"move", command.data.move_dir}
+		end,
+		executable = function(command, myplayer, tick)
+			if command.data.move_to_entity then
+				if not command.data.target_entity then
+					command.data.target_entity = get_entity_from_pos(command[2], myplayer)
+					if not command.data.target_entity then
+						return "No entity at " .. serpent.block(command.position) .. "."
+					else
+						command.rect = collision_box(command.data.target_entity)
+					end
+				end
+			elseif command.data.move_to_command then
+				if not command.data.target_command then
+					for _, com in pairs(global.command_list_parser.current_command_set) do
+						if com.name and has_value({command[2], command.namespace .. command[2]}, com.namespace .. com.name) then
+							command.data.target_command = com
+						end
+					end
+				end
+
+				if not command.data.target_command then
+					errprint("move: There is no command named: " .. command[2])
+					return "There is no command named: " .. command[2]
+				end
+
+				if command.data.target_command[1] == "craft-build" and command.data.target_command.data.build_command then
+					command.data.target_command = command.data.target_command.data.build_command
+				end
+
+				if not command.data.target_command.rect then
+					return "The command does currently not have a location"
+				end
 			end
 
-			if not command.data.target_command.rect then
-				return "The command does currently not have a location"
+			if (not command.data.target_pos or not command.data.move_started) then
+				if command[1] == "move"  then
+					if command.data.move_to_command then
+						command.data.target_pos = closest_point(command.data.target_command.rect, command.data.target_command.distance, myplayer.position)
+					elseif command.data.move_to_entity then
+						command.data.target_pos = closest_point(command.rect, command.distance, myplayer.position)
+					else
+						command.data.target_pos = command[2]
+					end
+				else
+					command.data.target_pos = closest_point(command.rect, command.distance, myplayer.position)
+				end
 			end
 
-			return auto_move_to_low_level(command, myplayer, tick)
+			if not command.data.last_dir then
+				command.data.last_dir = ""
+
+				if myplayer.position.y > command.data.target_pos[2] then
+					command.data.last_dir = command.data.last_dir .. "N"
+				end
+				if myplayer.position.y < command.data.target_pos[2] then
+					command.data.last_dir = command.data.last_dir .. "S"
+				end
+				if myplayer.position.x > command.data.target_pos[1] then
+					command.data.last_dir = command.data.last_dir .. "W"
+				end
+				if myplayer.position.x < command.data.target_pos[1] then
+					command.data.last_dir = command.data.last_dir .. "E"
+				end
+			end
+
+			local epsilon = 0.15 -- TODO: This should depend on the velocity.
+
+			if myplayer.position.y > command.data.target_pos[2] + epsilon then
+				command.data.move_north = true
+			end
+			if myplayer.position.y < command.data.target_pos[2] - epsilon then
+				command.data.move_south = true
+			end
+			if myplayer.position.x > command.data.target_pos[1] + epsilon then
+				command.data.move_west = true
+			end
+			if myplayer.position.x < command.data.target_pos[1] - epsilon then
+				command.data.move_east = true
+			end
+
+			if myplayer.position.y < command.data.target_pos[2] then
+				command.data.move_north = false
+			end
+			if myplayer.position.y > command.data.target_pos[2] then
+				command.data.move_south = false
+			end
+			if myplayer.position.x < command.data.target_pos[1] then
+				command.data.move_west = false
+			end
+			if myplayer.position.x > command.data.target_pos[1] then
+				command.data.move_east = false
+			end
+
+			command.data.move_dir = ""
+
+			if command.data.move_north then
+				command.data.move_dir = command.data.move_dir .. "N"
+			end
+			if command.data.move_south then
+				command.data.move_dir = command.data.move_dir .. "S"
+			end
+			if command.data.move_west then
+				command.data.move_dir = command.data.move_dir .. "W"
+			end
+			if command.data.move_east then
+				command.data.move_dir = command.data.move_dir .. "E"
+			end
+
+			return ""
 		end,
 		default_priority = 7,
 		initialize = function (command, myplayer)
@@ -734,23 +710,23 @@ high_level_commands = {
 			command.data.move_south = false
 			command.data.move_west = false
 			command.data.move_east = false
+			
+			command.distance = myplayer.reach_distance
 
 			if type(command[2]) == "string" then
 				command.data.move_to_command = true
-			else
-				return
-			end
-
-			if not command.data.target_command then
+				
 				for _, com in pairs(global.command_list_parser.current_command_set) do
 					if com.name and has_value({command[2], command.namespace .. command[2]}, com.namespace .. com.name) then
 						command.data.target_command = com
 					end
 				end
-			end
 
-			if not command.data.target_command and string.sub(command[2], 1, 3) == "bp_" then
-				global.high_level_commands.command_requests[#global.high_level_commands.command_requests + 1] = {command[2], command.namespace}
+				if not command.data.target_command and string.find(command[2], "bp_") then
+					global.high_level_commands.command_requests[#global.high_level_commands.command_requests + 1] = {command[2], command.namespace}
+				end
+			elseif command[2].entity then
+				command.data.move_to_entity = true
 			end
 		end,
 	},
