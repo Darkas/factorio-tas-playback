062d490352c16adac006d0a6c6ef8cefab0ed077
Andre Knispel <andre.knispel@gmx.de>
2017-11-03 18:01:02 +0100

    Added a warning if resouces are lost while changing a recipe.

--- commands.lua
+++ commands.lua
@@ -326,7 +326,12 @@ TAScommands["recipe"] =
             "Setting recipe: Entity at position {" .. tokens[2][1] .. "," .. tokens[2][2] .. "} could not be selected."
         )
         return
-    end
+	end
+	if myplayer.selected.crafting_progress then
+		if myplayer.selected.crafting_progress > 0 then
+			LogUI.errprint("Setting recipe: Entity at position {" .. tokens[2][1] .. "," .. tokens[2][2] .. "} was still crafting something. Items have been lost.")
+		end
+	end
     local ent =
         myplayer.surface.create_entity {
         name = myplayer.selected.name,
